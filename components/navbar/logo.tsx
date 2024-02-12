591095732fe915bdc6d8ff882478b172d8bef957
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-02-12 15:20:36 -0300

    feat: logo component

---
+++ components/navbar/logo.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+
+import React from 'react'
+
+function Logo() {
+
+  const router = useRouter()
+
+  return (
+    <div 
+    className="hidden md:block cursor-pointer text-purple-500 font-bold"
+    onClick={() => router.push('/')}
+    >
+        traveller
+    </div>
+  )
+}
+
+export default Logo
