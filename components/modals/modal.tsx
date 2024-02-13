f206f76cdea77f2e5889fed89cba714162640a89
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-02-13 01:12:46 -0300

    feat: general modal component

--- components/modals/modal.tsx
+++ components/modals/modal.tsx
@@ -2,7 +2,7 @@
 
 import React, { useCallback, useEffect, useState } from 'react'
 import { IoMdClose } from 'react-icons/io'
-import Button from './button'
+import Button from '../shared/button'
 
 type ModalProps = {
     isOpen?: boolean
@@ -68,7 +68,7 @@ function Modal({isOpen, onClose, onSubmit, title, body, footer, actionLabel, dis
   return (
     <>
         <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70'>
-            <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full md:h-auto'>
+            <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/6 my-6 mx-auto h-full md:h-auto'>
                 <div className={`translate duration-300 h-full
                 ${showModal ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}
                 `}>
@@ -94,6 +94,7 @@ function Modal({isOpen, onClose, onSubmit, title, body, footer, actionLabel, dis
                                     <Button label={actionLabel} disabled={disabled} onClick={handleSubmit} />
 
                                 </div>
+                                {footer}
                             </div>
                     </div>
                 </div>
