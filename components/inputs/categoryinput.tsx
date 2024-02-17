c87de8503f7cc71822e4908ca10b700c57b6793d
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-02-17 00:55:54 -0300

    feat: category selector component

---
+++ components/inputs/categoryinput.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+import { IconType } from 'react-icons'
+
+type CategoryInputProps = {
+  label: string
+  icon: IconType
+  selected?: boolean
+  onClick: (value: string) => void
+}
+
+function CategoryInput({label, icon: Icon, selected, onClick}: CategoryInputProps) {
+  return (
+    <div className={`rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer
+    ${selected? 'border-black' : 'border-neutral-200'}
+    `} 
+    onClick={() => onClick(label)}
+    >
+      <Icon size={30}/>
+      <div className='font-semibold'>{label}</div>
+    </div>
+  )
+}
+
+export default CategoryInput
