f790d122fd6402bc25d44655de6bb7085bbf5475
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-03-01 02:38:04 -0300

    feat: no results screen

---
+++ components/shared/emptystate.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import Heading from './heading'
+import Button from './button'
+
+type EmptyStateProps = {
+    title?: string
+    subtitle?: string
+    showReset?: boolean
+}
+
+function EmptyState({
+    title='Parece que não tem nada por aqui', 
+    subtitle='Tente mudar ou remover alguns filtros', 
+    showReset
+}: EmptyStateProps) {
+
+    const router = useRouter()
+
+  return (
+    <div className='h-[60vh] flex flex-col gap-2 justify-center items-center'>
+        <Heading center title={title} subtitle={subtitle} />
+        <div className='w-48 mt-4'>
+            {showReset && (
+                <Button
+                    outline
+                    label='Remover Filtros'
+                    onClick={() => router.push('/')}
+                />
+            )}
+        </div>
+    </div>
+  )
+}
+
+export default EmptyState
