02c2a169e0f7aa3e0655b4ec429913a643c2c802
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-03-11 01:01:46 -0300

    feat: uses search modal at navigation

--- components/navbar/search.tsx
+++ components/navbar/search.tsx
@@ -1,22 +1,71 @@
 'use client'
 
-import React from 'react'
+import useCountries from '@/hooks/useCountries'
+import useSearchModal from '@/hooks/useSearchModal'
+import { differenceInDays } from 'date-fns'
+import { useSearchParams } from 'next/navigation'
+import React, { useMemo } from 'react'
 import { BiSearch } from 'react-icons/bi'
 
 function Search() {
+
+    const searchModal = useSearchModal()
+    const params = useSearchParams()
+    const { getByValue } = useCountries()
+
+    const locationValue = params?.get('locationValue')
+    const startDate = params?.get('startDate')
+    const endDate = params?.get('endDate')
+    const guestCount = params?.get('guestCount')
+
+    const locationLabel = useMemo(() => {
+        if (locationValue) {
+            return getByValue(locationValue as string)?.label
+        }
+
+        return 'Onde'
+
+    }, [locationValue, getByValue])
+
+    const durationLabel = useMemo(() => {
+        if (startDate && endDate) {
+            const start = new Date(startDate as string)
+            const end = new Date(endDate as string)
+            let diff = differenceInDays(end, start)
+
+            if (diff === 0) {
+                diff = 1
+            }
+
+            return `${diff} dias` 
+        }
+
+        return 'Quando'
+    }, [startDate, endDate])
+
+    const guestLabel = useMemo(() => {
+        if (guestCount) {
+            return `${guestCount} hóspedes`
+        }
+
+        return 'Hóspedes'
+    }, [guestCount])
+
   return (
-    <div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
+    <div 
+    onClick={searchModal.onOpen}
+    className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
         <div className='flex items-center justify-between'>
             <div className='text-sm font-semibold px-6'>
-                Onde
+                {locationLabel}
             </div>
             <div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>
-                Quando
+                {durationLabel}
             </div>
             <div className='text-sm pl-6 pr-2 text-gray-600 flex items-center gap-3'
             >
                 <div className='hidden sm:block'>
-                    Hóspedes
+                    {guestLabel}
                 </div>
                 <div className='p-2 bg-purple-500 rounded-full text-white'>
                     <BiSearch size={18} />
